6d7577597d4e471e37c2a93242f242bbbb287e3a
ajbatista <ajbatistav@gmail.com>
2023-06-15 14:27:37 -0400

    add cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express  from "express";
 import productsRoute from './route/products.routes.js';
 import indexRoutes from './route/index.routes.js';
 import ventasRoute from './route/ventas.routes.js';
+import cors from 'cors';
+
 const app = express();
 import {PORT} from './config.js';
 
@@ -9,6 +11,12 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use(indexRoutes);
 app.use(productsRoute);
